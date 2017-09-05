8d2e558ad4cdd1c60453c91b9353803c9a111052
Tinywan <756684177@qq.com>
2017-09-05 17:47:01 +0800

    默认跳转页面对应的模板文件

--- config/config.php
+++ config/config.php
@@ -4,7 +4,7 @@ return [
     'app_author'          => 'Tinywan',
     'app_email'          => '756684177@qq.com',
     'app_debug'              => true,
-//    // 入口自动绑定模块
+     // 入口自动绑定模块
 //    'auto_bind_module'       => true,
 //    // 是否开启路由
     'url_route_on'           => true,

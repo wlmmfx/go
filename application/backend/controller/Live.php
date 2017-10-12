9c5db83e5219d2bf248478359654528a012dccdb
Tinywan <756684177@qq.com>
2017-10-12 18:11:06 +0800

    update left banner css style

--- application/backend/controller/Live.php
+++ application/backend/controller/Live.php
@@ -17,12 +17,38 @@ use think\Db;
 
 class Live extends BaseBackend
 {
+    const AUTH_PRIVATEKEY = 'Tinywan123';
     protected $db;
 
     public function _initialize()
     {
         parent::_initialize();
         $this->db = new \app\common\model\Live();
+    }
+
+    /**
+     * 获取推流地址auth_key
+     * @param string $sourceName
+     * @param string $domainName
+     * @param string $appName
+     * @param $streamName
+     * @param $startTime
+     * @param $expireTime
+     * @param string $cdn
+     * @return mixed
+     */
+    public static function getPushUrlAuthKey($sourceName='live.tinywan.com', $domainName='live.tinywan.com', $appName='live', $streamName, $startTime, $expireTime, $cdn='live.tinywan.com')
+    {
+        $auth_key = self::AUTH_PRIVATEKEY;
+        $timestatmp = strtotime(date('Y-m-d H:i:s', strtotime($startTime . "+" . $expireTime . " minute ")));
+        $rtmp_auth_md5 = md5("/" . $appName . "/" . $streamName . "-" . $timestatmp . "-0-0-" . $auth_key);
+        $hls_auth_md5 = md5("/" . $appName . "/" . $streamName . ".m3u8-" . $timestatmp . "-0-0-" . $auth_key);
+
+        $authUrl['push_flow_address'] = "rtmp://$sourceName/$appName/$streamName?vhost=$domainName&auth_key=" . $timestatmp . "-0-0-" . $rtmp_auth_md5;
+        $authUrl['play_rtmp_address'] = "rtmp://$domainName/$appName/$streamName?auth_key=" . $timestatmp . "-0-0-" . $rtmp_auth_md5;
+        $authUrl['play_m3u8_address'] = "http://$cdn/$appName/$streamName.m3u8?auth_key=" . $timestatmp . "-0-0-" . $hls_auth_md5;
+        $authUrl['hash_value'] = 'startTime = ' . $startTime . ' expireTime = ' . $expireTime . ' timestatmp = ' . $timestatmp . "uri = /" . $appName . "/" . $streamName . ".m3u8-" . $timestatmp . "-0-0-" . $auth_key;
+        return $authUrl;
     }
 
     /**
@@ -50,6 +76,8 @@ class Live extends BaseBackend
     public function liveStore()
     {
         if (request()->isPost()) {
+            $streamName = '400' . time();
+            $startTime = date('Y-m-d H:i:s', time());
             $res = $this->db->store(input('post.'));
             if ($res["valid"]) {
                 $this->success($res["msg"], "backend/live/liveIndex");

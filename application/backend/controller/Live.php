7ccbda866e73d6ee1f008fc6902dc0a07970704f
Tinywan <756684177@qq.com>
2017-10-24 17:45:27 +0800

    Nice Upload Max-800M Video File & Play upload video

--- application/backend/controller/Live.php
+++ application/backend/controller/Live.php
@@ -12,9 +12,13 @@
 namespace app\backend\controller;
 
 
+use aliyun\oss\OssInstance;
 use app\common\controller\BaseBackend;
-use houdunwang\arr\Arr;
+use \FFMpeg\Coordinate\TimeCode;
+use FFMpeg\FFMpeg;
+use OSS\Core\OssException;
 use think\Db;
+use think\Log;
 
 class Live extends BaseBackend
 {
@@ -38,7 +42,7 @@ class Live extends BaseBackend
      * @param string $cdn
      * @return mixed
      */
-    public static function getPushUrlAuthKey($sourceName='live.tinywan.com', $domainName='live.tinywan.com', $appName='live', $streamName, $startTime, $expireTime, $cdn='live.tinywan.com')
+    public static function getPushUrlAuthKey($sourceName = 'live.tinywan.com', $domainName = 'live.tinywan.com', $appName = 'live', $streamName, $startTime, $expireTime, $cdn = 'live.tinywan.com')
     {
         $auth_key = self::AUTH_PRIVATEKEY;
         $timestatmp = strtotime(date('Y-m-d H:i:s', strtotime($startTime . "+" . $expireTime . " minute ")));
@@ -72,7 +76,7 @@ class Live extends BaseBackend
             ->field("l.id,l.liveStartTime,l.name,l.createTime,l.liveEndTime,l.recordStatus,c.name as c_name")
             ->order("l.createTime desc,l.liveStartTime desc")
             ->paginate(4);
-        $categorys = db('category')->where('pid',116)->order('id desc')->select();
+        $categorys = db('category')->where('pid', 116)->order('id desc')->select();
         $this->assign('lives', $lives);
         $this->assign('categorys', $categorys);
         return $this->fetch();
@@ -103,7 +107,27 @@ class Live extends BaseBackend
     public function liveDetail()
     {
         $liveId = input('param.id');
-        $videos = Db::table('resty_stream_video')->where('streamName',$liveId)->order('createTime desc')->paginate(12);
+        $videos = Db::table('resty_stream_video')->order('createTime desc')->paginate(12);
+        $this->assign('videos', $videos);
+        return $this->fetch();
+    }
+
+    /**
+     * 视频管理
+     */
+    public function videoManage()
+    {
+        $videos = Db::table('resty_stream_video')->order('createTime desc')->paginate(12);
+        $this->assign('videos', $videos);
+        return $this->fetch();
+    }
+
+    /**
+     * 素材管理，上传视频管理
+     */
+    public function uploadVideoManage()
+    {
+        $videos = db('stream_video')->where('type',2)->order('id desc')->select();
         $this->assign('videos', $videos);
         return $this->fetch();
     }
@@ -129,7 +153,7 @@ class Live extends BaseBackend
             halt($liveStartTime);
         }
         $liveId = input('param.id');
-        $videos = Db::table('resty_stream_video')->where('streamName',$liveId)->order('createTime desc')->paginate(12);
+        $videos = Db::table('resty_stream_video')->where('streamName', $liveId)->order('createTime desc')->paginate(12);
         $this->assign('videos', $videos);
         return $this->fetch();
     }
@@ -155,7 +179,7 @@ class Live extends BaseBackend
      */
     public function videoUploadFrom()
     {
-        if(request()->isPost()){
+        if (request()->isPost()) {
             $file = request()->file("myfile");
             if ($file) {
                 $data = input('post.*');
@@ -182,31 +206,97 @@ class Live extends BaseBackend
         return $this->fetch();
     }
 
+    /**
+     * 截图操作
+     */
+    public static function cutImageHandle($fileTmpPath, $cutImageName, $savePath,$cutImageTime)
+    {
+        $video = self::ffmpeg()->open($fileTmpPath);
+        //截取视频图片 2s 时候截取
+        $video->frame(TimeCode::fromSeconds($cutImageTime))
+            ->save($savePath . DS. $cutImageName);
+        return $cutImageName;
+    }
+
+    /**
+     * 素材视频图片上传
+     * @return mixed|\think\response\Json
+     */
     public function liveVideoUploadFrom()
     {
-        if(request()->isPost()){
+        if (request()->isPost()) {
             $file = request()->file("video_file");
             if ($file) {
                 $id = input('post.id');
-                $info = $file->rule("uniqid")->move(ROOT_PATH . 'public' . DS . 'uploads/videos');
+                $upload_desc = input('post.upload_desc');
+                $cutImageTime = input('post.cut_image_time');
+                $startPath = ROOT_PATH . 'public' . DS . 'uploads/videos/'. $id ;
+                // 根据唯一标识目录创建，这里应该判断是否具有权限创建
+                if (!is_dir($startPath)) mkdir($startPath);
+                if (!is_dir($startPath. "/video")) mkdir($startPath. "/video");
+                $savePath = $startPath. "/video";
+                $info = $file->rule("uniqid")->move($savePath);
                 if ($info) {
                     // 成功上传后 获取上传信息
-                    $fileTmpPath = ROOT_PATH . 'public' . DS . 'uploads/videos' . DS . $info->getSaveName();
-                    $res = [
-                        'code' => 200,
-                        'msg' => '恭喜你，上传成功',
-                        'data' => [
-                            'Id' => $id,
-                            'Extension' => $info->getExtension(),
-                            'Filename' => $info->getFilename(),
-                            'Size' => trans_byte(self::getVideoSize($fileTmpPath)),
-                            'Duration' => gmstrftime('%H:%M:%S',self::getVideoDuration($fileTmpPath)),
-                            'width' => self::ffprobe()->streams($fileTmpPath)->videos()->first()->get('width'),
-                            'height' => self::ffprobe()->streams($fileTmpPath)->videos()->first()->get('height'),
-                            'codec_type' => self::ffprobe()->streams($fileTmpPath)->videos()->first()->get('codec_type'),
-                            'codec_long_name' => self::ffprobe()->streams($fileTmpPath)->videos()->first()->get('codec_long_name'),
-                        ]
+                    $fileTmpPath = $savePath . DS . $info->getSaveName();
+                    $baseName = $info->getFilename();
+                    $ext = $info->getExtension();
+                    $fileSize = self::getVideoSize($fileTmpPath);
+                    $duration = gmstrftime('%H:%M:%S', self::getVideoDuration($fileTmpPath));
+                    $cutImageName = pathinfo($fileTmpPath)['filename'] . '.jpg';
+                    $screenshotName = self::cutImageHandle($fileTmpPath, $cutImageName, $savePath,$cutImageTime);
+
+                    $videoData = [
+                        'streamName' => $id,
+                        'channelId' => $id,
+                        'name' => $baseName,
+                        'type' => 2, // 1.录像 2.上传 3.编辑
+                        'fileName' => $baseName,
+                        'fileTime' => date("Y-m-d H:i:s"),
+                        'fileSize' => $fileSize,
+                        'duration' => $duration,
+                        'createTime' => date("Y-m-d H:i:s"),
+                        'screenshot' => $screenshotName,
+                        'desc' => $upload_desc,
                     ];
+
+                    //oss upload
+                    $oss = OssInstance::Instance();
+                    $bucket = config('aliyun_oss.BUCKET');
+                    $ossbObject = 'data/' . $id.'/video';
+                    try {
+                        $oss->uploadDir($bucket, $ossbObject, $savePath.DS);
+                        $videoData['oss_upload_status'] = 1;
+                    } catch (OssException $e) {
+                        $videoData['oss_upload_status'] = json_encode($e->getMessage());
+                    }
+                    //插入数据库
+                    $insertRes = Db::table('resty_stream_video')->insertGetId($videoData);
+                    if ($insertRes) {
+                        // 遍历删除原图和缩略图
+                        $res = [
+                            'code' => 200,
+                            'msg' => '恭喜你，上传成功',
+                            'data' => [
+                                'Id' => $id,
+                                'Extension' => $ext,
+                                'Filename' => $baseName,
+                                'Size' => $fileSize,
+                                'Duration' => $duration,
+                                'width' => self::ffprobe()->streams($fileTmpPath)->videos()->first()->get('width'),
+                                'height' => self::ffprobe()->streams($fileTmpPath)->videos()->first()->get('height'),
+                                'codec_type' => self::ffprobe()->streams($fileTmpPath)->videos()->first()->get('codec_type'),
+                                'codec_long_name' => self::ffprobe()->streams($fileTmpPath)->videos()->first()->get('codec_long_name'),
+                            ]
+                        ];
+                        $this->rmdirs($savePath);
+                    } else {
+                        $res = [
+                            'code' => 500,
+                            'msg' => '上传成功，插入数据库失败',
+                            'data' => null
+                        ];
+                    }
                 } else {
                     $res = [
                         'code' => 500,

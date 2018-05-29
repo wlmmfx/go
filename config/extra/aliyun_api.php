a06ffe68d4417c7bfcb9c57868a05bf5c29e360d
Tinywan <756684177@qq.com>
2018-05-29 22:21:04 +0800

    reslove aliyun config

---
+++ config/extra/aliyun_api.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * User: tinywan
+ * Date: 2017/6/23
+ * Time: 22:07
+ * ****************************OSS 信息**********************************
+ */
+return [
+    'ACCESSKEYID'        => '', // 阿里云oss key_id
+    'ACCESSKEYSECRET'    => '', // 阿里云oss key_secret
+    'CDN_SERVER_ADDRESS'             => 'https://cdn.aliyuncs.com',
+    'ALI_AUTH_DOMAIN'             => 'https://cdn.aliyuncs.com',
+    'ALI_AUTH_PRIVATEKEY'             => 'https://cdn.aliyuncs.com',
+    'CENTER_STREAM_ADDRESS'             => 'stream.tinywan.com', //stream.tinywan.com CNAME 到 video-center.alivecdn.com
+];
